f830e0b73e3244699f9cc99a5cd667184c97c412
Dallas <dlarsen@looominance.com>
2023-01-17 20:25:19 -0800

    Add S\simple test folder

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
 
-import { SideDrawer } from './src';
+import { SideDrawer } from '../src';
 
 describe('Simple common render', () => {
   it('renders without crashing', () => {
